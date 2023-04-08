7a2e00623d5c255318d8b18b54df65504fbb3460
Jesse Emerson <jesseemerson7@gmail.com>
2023-04-07 23:55:09 -0400

    added html and css styling to weather page ready for fetch js

---
+++ assets/js/script.js
@@ -0,0 +1,2 @@
+// http://api.openweathermap.org/geo/1.0/direct?q=Orlando&limit=5&appid=bbbb7f099358f595a34abd46aedb1c99
+//fetch request for coords based on users search. Need to connect users input from the input and sent request with that value. get lat and long from that request and then use that lat and long to get weather api weather forecast to display on page.
